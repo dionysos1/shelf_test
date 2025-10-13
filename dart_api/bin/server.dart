fc8b851ac96a1b6c37c741b0ac5f4a6ccff02740
Dion van den Berg <dionberg@gmail.com>
2025-10-13 13:37:41 +0200

    Add files via upload

--- dart_api/bin/server.dart
+++ dart_api/bin/server.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:dart_api/users_ui.dart';
@@ -7,19 +6,11 @@ import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_router/shelf_router.dart';
 import 'package:dart_api/db.dart';
 
-Future<Response> getUsersHandler(Request request) async {
-  final users = await Db.query('SELECT id, name FROM users');
-  return Response.ok(jsonEncode(users), headers: {
-    'Content-Type': 'application/json',
-  });
-}
-
 Future<void> main() async {
   await Db.init();
 
   final router = Router()
-    ..get('/', (req) => Response.ok('API is running'))
-    ..get('/users', getUsersHandler)
+    ..get('/', (req) => getHomePage)
     ..mount('/', usersUiRouter().call);
 
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(router.call);
@@ -27,4 +18,22 @@ Future<void> main() async {
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await io.serve(handler, InternetAddress.anyIPv4, port);
   print('API running on http://localhost:${server.port}');
+}
+
+Response get getHomePage {
+  return Response.ok(
+    '''
+    <html>
+      <head><title>Dart API</title></head>
+      <body>
+        <h2>Available Routes</h2>
+        <ul>
+          <li><a href="/users-ui">User Management UI</a></li>
+          <li><a href="/api/users">Users API (GET)</a></li>
+        </ul>
+      </body>
+    </html>
+    ''',
+    headers: {'Content-Type': 'text/html'},
+  );
 }
